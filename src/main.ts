8371c709540ed40a8a3da8756c3198406f454681
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-05-19 04:45:22 +1200

    Uncomment out

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { assert } from "console";
 import { Message, Client } from "discord.js";
 import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
 const client = new Client({
     intents: ["GUILDS", "GUILD_MESSAGES"],
